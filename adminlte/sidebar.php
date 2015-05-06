a64b4659232b9832bf20b5f0c06f07d6b9bca629
oh001738 <oh001738@gmail.com>
2015-05-06 16:04:17 +0800

    加一點美妙的小圖片

--- adminlte/sidebar.php
+++ adminlte/sidebar.php
@@ -22,7 +22,7 @@
 					{
 						echo "<li class='header'>醫事人員</li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php'><i class='fa fa-tachometer fa-fw'></i>儀錶板</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food_element'><i class='fa fa-cutlery fa-fw'></i>".(($USER['power'] == '3')?("維護食材"):("檢視食材"))."</a></li>\n";
+						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food_element'><i class='fa fa-flask fa-fw'></i>".(($USER['power'] == '3')?("維護食材"):("檢視食材"))."</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food'><i class='fa fa-cutlery fa-fw'></i>".(($USER['power'] == '3')?("維護食物"):("檢視食物"))."</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=suitfood'><i class='fa fa-shopping-cart fa-fw'></i>".(($USER['power'] == '3')?("維護套餐"):("檢視套餐"))."</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=user'><i class='fa fa-table fa-fw'></i>".(($USER['power'] == '3')?("維護個人資料"):("檢視個人資料"))."</a></li>\n";
@@ -31,14 +31,14 @@
 					}else{
 						echo "<li class='header'>管理人員</li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php'><i class='fa fa-tachometer fa-fw'></i>儀錶板</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food_element'>維護食材元素</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food'>維護食物</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=suitfood'>維護套餐</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=user'>維護個人資料</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=attention'>維護個案照顧記錄</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=power'>權限管理</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=actionview'>檢視行為紀錄</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/logout.php'>登出</a></li>\n";
+						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food_element'><i class='fa fa-flask fa-fw'></i>維護食材元素</a></li>\n";
+						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food'><i class='fa fa-cutlery fa-fw'></i>維護食物</a></li>\n";
+						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=suitfood'><i class='fa fa-shopping-cart fa-fw'></i>維護套餐</a></li>\n";
+						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=user'><i class='fa fa-table fa-fw'></i>維護個人資料</a></li>\n";
+						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=attention'><i class='fa fa-user fa-fw'></i>維護個案照顧記錄</a></li>\n";
+						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=power'><i class='fa fa-linux fa-fw'></i>權限管理</a></li>\n";
+						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=actionview'><i class='fa fa-terminal fa-fw'></i>檢視行為紀錄</a></li>\n";
+						echo "<li><a href = '" . ROOT_URL . "/logout.php'><i class='fa fa-sign-out fa-fw'></i>登出</a></li>\n";
 
 					}
 					?>
