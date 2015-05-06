e6605c1be5d43c74320c0946c2c3473312af200f
Larry Tsai <oh001738@gmail.com>
2015-05-06 22:07:56 +0800

    Revert "Revert "取消套餐功能""
    This reverts commit 2659ffa53324fb41602b69cd8b4e514e364a1ade.

--- adminlte/sidebar.php
+++ adminlte/sidebar.php
@@ -24,7 +24,7 @@
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php'><i class='fa fa-tachometer fa-fw'></i>儀錶板</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food_element'><i class='fa fa-flask fa-fw'></i>".(($USER['power'] == '3')?("維護食材"):("檢視食材"))."</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food'><i class='fa fa-cutlery fa-fw'></i>".(($USER['power'] == '3')?("維護食物"):("檢視食物"))."</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=suitfood'><i class='fa fa-shopping-cart fa-fw'></i>".(($USER['power'] == '3')?("維護套餐"):("檢視套餐"))."</a></li>\n";
+						//取消套餐功能 echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=suitfood'><i class='fa fa-shopping-cart fa-fw'></i>".(($USER['power'] == '3')?("維護套餐"):("檢視套餐"))."</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=user'><i class='fa fa-table fa-fw'></i>".(($USER['power'] == '3')?("維護個人資料"):("檢視個人資料"))."</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=attention'><i class='fa fa-user-plus fa-fw'></i>維護個案照顧記錄</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/logout.php'><i class='fa fa-sign-out fa-fw'></i>登出</a></li>\n";
@@ -33,7 +33,7 @@
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php'><i class='fa fa-tachometer fa-fw'></i>儀錶板</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food_element'><i class='fa fa-flask fa-fw'></i>維護食材元素</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=food'><i class='fa fa-cutlery fa-fw'></i>維護食物</a></li>\n";
-						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=suitfood'><i class='fa fa-shopping-cart fa-fw'></i>維護套餐</a></li>\n";
+						//取消套餐功能 echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=suitfood'><i class='fa fa-shopping-cart fa-fw'></i>維護套餐</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=user'><i class='fa fa-table fa-fw'></i>維護個人資料</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=attention'><i class='fa fa-user fa-fw'></i>維護個案照顧記錄</a></li>\n";
 						echo "<li><a href = '" . ROOT_URL . "/adminlte/index.php?op=power'><i class='fa fa-linux fa-fw'></i>權限管理</a></li>\n";
